21ac69de4f16aeb676e9a1f69684aea1045c60d4
Michael Hahn <mwhahn@gmail.com>
2025-05-01 22:24:46 -0700

    Support gesture forwarding even in scroll views

--- Lexical/LexicalView/LexicalOverlayView.swift
+++ Lexical/LexicalView/LexicalOverlayView.swift
@@ -4,133 +4,133 @@
 //
 //  Created by Michael Hahn on 7/30/24.
 //
-
-import Foundation
 import UIKit
 
-class LexicalOverlayView: UIView {
+final class LexicalOverlayView: UIView {
+
   private weak var textView: UITextView?
+  private lazy var tapRecognizer: UITapGestureRecognizer = {
+    let gr = UITapGestureRecognizer(target: self, action: #selector(didTap(_:)))
+    gr.cancelsTouchesInView = false  // keep scroll-view panning intact
+    return gr
+  }()
+
+  // MARK: – Init
 
   init(textView: UITextView) {
     self.textView = textView
     super.init(frame: .zero)
-    self.backgroundColor = .clear  // Make it transparent
+    backgroundColor = .clear
+    addGestureRecognizer(tapRecognizer)
   }
 
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+  @available(*, unavailable)
+  required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+  // MARK: – Hit-testing
 
   override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-    if shouldInterceptTap(at: point) {
-      return self
-    }
-
-    return textView?.hitTest(convert(point, to: textView), with: event)
-      ?? super.hitTest(point, with: event)
+    return shouldInterceptTap(at: point)
+      ? self
+      : textView?.hitTest(convert(point, to: textView), with: event)
+        ?? super.hitTest(point, with: event)
   }
 
-  func shouldInterceptTap(at point: CGPoint) -> Bool {
-    guard let textView = textView as? TextView,
+  private func shouldInterceptTap(at point: CGPoint) -> Bool {
+    guard
+      let textView = textView as? TextView,
       let textStorage = textView.textStorage as? TextStorage
     else { return false }
 
+    // Convert to text-container coordinates
     let pointInTextView = convert(point, to: textView)
-
-    let pointInTextContainer = CGPoint(
+    let pointInContainer = CGPoint(
       x: pointInTextView.x - textView.textContainerInset.left,
       y: pointInTextView.y - textView.textContainerInset.top
     )
 
-    let indexOfCharacter = textView.layoutManager.characterIndex(
-      for: pointInTextContainer,
-      in: textView.textContainer,
-      fractionOfDistanceBetweenInsertionPoints: nil
+    // Hit-test glyph line & attributes
+    let lm = textView.layoutManager
+    let glyph = lm.glyphIndex(for: pointInContainer, in: textView.textContainer)
+    guard
+      lm.lineFragmentRect(forGlyphAt: glyph, effectiveRange: nil)
+        .contains(pointInContainer)
+    else { return false }
+
+    let attrs = textStorage.attributes(
+      at: lm.characterIndex(
+        for: pointInContainer,
+        in: textView.textContainer,
+        fractionOfDistanceBetweenInsertionPoints: nil),
+      effectiveRange: nil
     )
-    let glyphIndex = textView.layoutManager.glyphIndex(
-      for: pointInTextContainer, in: textView.textContainer)
-    let lineFragmentRect = textView.layoutManager.lineFragmentRect(
-      forGlyphAt: glyphIndex, effectiveRange: nil)
-    let firstCharacterRect = textView.layoutManager.boundingRect(
-      forGlyphRange: NSRange(location: glyphIndex, length: 1),
-      in: textView.textContainer
-    )
-    if !lineFragmentRect.contains(pointInTextContainer) {
-      return false
-    }
 
-    let attributes = textStorage.attributes(at: indexOfCharacter, effectiveRange: nil)
-    for plugin in textView.editor.plugins {
-      if let hit = plugin.hitTest?(
-        at: pointInTextContainer, lineFragmentRect: lineFragmentRect,
-        firstCharacterRect: firstCharacterRect, attributes: attributes), hit
-      {
-        return true
-      }
-    }
-    return false
-  }
-
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print("🟢 touchesBegan")
-    super.touchesBegan(touches, with: event)
-  }
-
-  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print("🔴 touchesCancelled")
-    super.touchesCancelled(touches, with: event)
-  }
-
-  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    super.touchesEnded(touches, with: event)
-    if let touch = touches.first {
-      let location = touch.location(in: self)
-      handleTouchEvent(at: location)
+    // Ask plugins
+    return textView.editor.plugins.contains { plugin in
+      plugin.hitTest?(
+        at: pointInContainer,
+        lineFragmentRect: lm.lineFragmentRect(forGlyphAt: glyph, effectiveRange: nil),
+        firstCharacterRect: lm.boundingRect(
+          forGlyphRange: NSRange(location: glyph, length: 1),
+          in: textView.textContainer
+        ),
+        attributes: attrs
+      ) ?? false
     }
   }
 
-  private func handleTouchEvent(at point: CGPoint) {
-    guard let textView = textView as? TextView,
+  // MARK: – Tap handling
+
+  @objc private func didTap(_ gr: UITapGestureRecognizer) {
+    guard gr.state == .ended else { return }
+    forwardTapToPlugins(at: gr.location(in: self))
+  }
+
+  private func forwardTapToPlugins(at point: CGPoint) {
+    guard
+      let textView = textView as? TextView,
       let textStorage = textView.textStorage as? TextStorage
     else { return }
 
     let pointInTextView = convert(point, to: textView)
-
-    let pointInTextContainer = CGPoint(
+    let pointInContainer = CGPoint(
       x: pointInTextView.x - textView.textContainerInset.left,
       y: pointInTextView.y - textView.textContainerInset.top
     )
 
-    let indexOfCharacter = textView.layoutManager.characterIndex(
-      for: pointInTextContainer,
-      in: textView.textContainer,
-      fractionOfDistanceBetweenInsertionPoints: nil
-    )
-    let attributes = textStorage.attributes(at: indexOfCharacter, effectiveRange: nil)
-    let glyphIndex = textView.layoutManager.glyphIndex(
-      for: pointInTextContainer, in: textView.textContainer)
-    let lineFragmentRect = textView.layoutManager.lineFragmentRect(
-      forGlyphAt: glyphIndex, effectiveRange: nil)
-    let firstCharacterRect = textView.layoutManager.boundingRect(
-      forGlyphRange: NSRange(location: glyphIndex, length: 1),
-      in: textView.textContainer
+    let lm = textView.layoutManager
+    let glyph = lm.glyphIndex(for: pointInContainer, in: textView.textContainer)
+    let attrs = textStorage.attributes(
+      at: lm.characterIndex(
+        for: pointInContainer,
+        in: textView.textContainer,
+        fractionOfDistanceBetweenInsertionPoints: nil),
+      effectiveRange: nil
     )
 
     for plugin in textView.editor.plugins {
-      if let hit = plugin.hitTest?(
-        at: pointInTextContainer,
-        lineFragmentRect: lineFragmentRect,
-        firstCharacterRect: firstCharacterRect,
-        attributes: attributes
-      ), hit {
-        if let handled = plugin.handleTap?(
-          at: pointInTextContainer,
-          lineFragmentRect: lineFragmentRect,
-          firstCharacterRect: firstCharacterRect,
-          attributes: attributes
-        ), handled {
-          break
-        }
+      guard
+        plugin.hitTest?(
+          at: pointInContainer,
+          lineFragmentRect: lm.lineFragmentRect(forGlyphAt: glyph, effectiveRange: nil),
+          firstCharacterRect: lm.boundingRect(
+            forGlyphRange: NSRange(location: glyph, length: 1),
+            in: textView.textContainer
+          ),
+          attributes: attrs
+        ) ?? false
+      else { continue }
+
+      if plugin.handleTap?(
+        at: pointInContainer,
+        lineFragmentRect: lm.lineFragmentRect(forGlyphAt: glyph, effectiveRange: nil),
+        firstCharacterRect: lm.boundingRect(
+          forGlyphRange: NSRange(location: glyph, length: 1),
+          in: textView.textContainer
+        ),
+        attributes: attrs
+      ) ?? false {
+        break
       }
     }
   }
